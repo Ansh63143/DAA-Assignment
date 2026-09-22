517781e867f5b95781fdf08caaaa2ccc424a9bee
Ansh Gandhi <gandhiansh4@gmail.com>
2026-09-22 23:12:24 +0530

    Implement merge function for sorted arrays

---
+++ LC-88.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = m -1;
+        int r = n - 1;
+        int high = m + n -1;
+        while(l>= 0 && r >= 0){
+            if(nums1[l] < nums2[r]){
+                nums1[high--] = nums2[r--];
+            }
+            else{
+                nums1[high--] = nums1[l--];
+            }
+        }
+        while(r>=0){
+            nums1[high--] = nums2[r--];
+        }
+    }
+}
